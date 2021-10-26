4037ecd42507573dc187422dd333089115216a54
VizorTank <36300779+VizorTank@users.noreply.github.com>
2021-10-26 19:05:13 +0200

    Compleated

--- app/src/main/java/com/example/android1/MainActivity.java
+++ app/src/main/java/com/example/android1/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android1;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,10 +16,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String KEY_EXTRA_ANSWER = "pl.edu.pb.wi.quiz.correctAnswer";
+
+    private static final String QUIZ_TAG = "Main Activity";
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    private static final int REQUEST_CODE_PROMPT = 1;
+
 
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
+    private Button helpButton;
     private TextView questionTextView;
 
     private Question[] questions = new Question[] {
@@ -27,15 +38,60 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private int currentIndex = 0;
+    private boolean answerWasShown;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(QUIZ_TAG, "Wywołana została metoda: onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onCreate");
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null)
+        {
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
 
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
+        helpButton = findViewById(R.id.help_button);
         questionTextView = findViewById(R.id.question_text_view);
 
         trueButton.setOnClickListener(new View.OnClickListener() {
@@ -56,21 +112,49 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 currentIndex = (currentIndex + 1) % questions.length;
+                answerWasShown = false;
                 setNextQuestion();
             }
         });
+
+        helpButton.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+            boolean correctAnswer = questions[currentIndex].trueAnswer;
+            intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+            startActivityForResult(intent, REQUEST_CODE_PROMPT);
+        });
+        setNextQuestion();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(QUIZ_TAG, "Wywołana została metoda: onActivityResult " + REQUEST_CODE_PROMPT);
+        if (resultCode != RESULT_OK) { return; }
+        if (resultCode == -1)
+        {
+            if (data == null) { return; }
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+        }
     }
 
     private void checkAnswerCorrectness(boolean userAnswer) {
         boolean correctAnswer = questions[currentIndex].trueAnswer;
         int resultMessageId = 0;
-        if (userAnswer == correctAnswer)
+        if (answerWasShown)
         {
-            resultMessageId = R.string.correct_answer;
+            resultMessageId = R.string.answer_was_shown;
         }
         else
         {
-            resultMessageId = R.string.incorrect_answer;
+            if (userAnswer == correctAnswer)
+            {
+                resultMessageId = R.string.correct_answer;
+            }
+            else
+            {
+                resultMessageId = R.string.incorrect_answer;
+            }
         }
         Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
     }
---
+++ app/src/main/java/com/example/android1/PromptActivity.java
@@ -0,0 +1,50 @@
+package com.example.android1;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PromptActivity extends AppCompatActivity {
+
+    private boolean correctAnswer;
+    public static final String KEY_EXTRA_ANSWER_SHOWN = "answerShown";
+
+    private Button answerButton;
+    private TextView answerTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prompt);
+
+        correctAnswer = getIntent().getBooleanExtra(MainActivity.KEY_EXTRA_ANSWER, true);
+
+        answerButton = findViewById(R.id.answer_button);
+        answerTextView = findViewById(R.id.answer_text_view);
+
+        answerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int answer = correctAnswer ? R.string.button_true : R.string.button_false;
+
+                answerTextView.setText(answer);
+                setAnswerShowResult(true);
+            }
+        });
+        setAnswerShowResult(true);
+
+    }
+
+    private void setAnswerShowResult(boolean answerWasShown)
+    {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(KEY_EXTRA_ANSWER_SHOWN, answerWasShown);
+        setResult(RESULT_OK, resultIntent);
+    }
+
+}
